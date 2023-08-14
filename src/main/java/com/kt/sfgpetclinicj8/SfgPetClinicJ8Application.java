50046cac78ab0e42ce15ef9a6a8cfeb7916af79d
kontelk <ktelkis@gmail.com>
2023-08-14 09:00:37 +0300

    Initial commit

---
+++ src/main/java/com/kt/sfgpetclinicj8/SfgPetClinicJ8Application.java
@@ -0,0 +1,13 @@
+package com.kt.sfgpetclinicj8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicJ8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicJ8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kt/sfgpetclinicj8/SfgPetClinicJ8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.sfgpetclinicj8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicJ8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
