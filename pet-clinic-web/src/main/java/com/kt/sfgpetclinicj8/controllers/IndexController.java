249fa7e027ef411ce8e031e278fcbb477ea7dabf
kontelk <ktelkis@gmail.com>
2023-08-19 08:59:41 +0300

    Adding index page and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/com/kt/sfgpetclinicj8/controllers/IndexController.java
@@ -0,0 +1,26 @@
+package com.kt.sfgpetclinicj8.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by kontelk on 8/19/23.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping({"help"})
+    public String help() {
+        return "help";
+    }
+
+    @RequestMapping({"support"})
+    public String support() {
+        return "support";
+    }
+}
