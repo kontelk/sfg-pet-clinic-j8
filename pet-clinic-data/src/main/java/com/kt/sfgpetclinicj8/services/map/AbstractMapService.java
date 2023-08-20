7634d0e7ca4bcb757902700ef44b8d6b2bff2779
kontelk <ktelkis@gmail.com>
2023-08-20 10:08:13 +0300

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-data/src/main/java/com/kt/sfgpetclinicj8/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/kt/sfgpetclinicj8/services/map/AbstractMapService.java
@@ -1,16 +1,16 @@
 package com.kt.sfgpetclinicj8.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.kt.sfgpetclinicj8.model.BaseEntity;
+
+import java.util.*;
 
 /**
  * Created by kontelk on 8/18/23.
  */
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<
+        T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -20,8 +20,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }
+        else {
+            throw new RuntimeException("ERROR: Can not save a null Object");
+        }
         return object;
     }
 
@@ -31,5 +39,26 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    /**
+     * Helper method to get the next long number
+     * to assign it on id when we save a new object.
+     */
+    private Long getNextId() {
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet())+1;
+        }
+        catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
+        // ---------other way to do this------------- //
+        // if (map.size()==0)
+        //    return 1L;
+        // else
+        //    return Collections.max(map.keySet())+1;
+        // ----------------------------------------- //
     }
 }
