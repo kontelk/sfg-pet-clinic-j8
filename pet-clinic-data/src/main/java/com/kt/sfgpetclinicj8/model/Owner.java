6918850ef3987d9766db49c41252127fdc10025d
kontelk <ktelkis@gmail.com>
2023-08-23 09:24:25 +0300

    added visits, updated owner with pet relationship. closes #26

--- pet-clinic-data/src/main/java/com/kt/sfgpetclinicj8/model/Owner.java
+++ pet-clinic-data/src/main/java/com/kt/sfgpetclinicj8/model/Owner.java
@@ -1,9 +1,19 @@
 package com.kt.sfgpetclinicj8.model;
 
+import java.util.Set;
+
 /**
  * Created by kontelk on 8/15/23.
  */
 public class Owner extends Person {
 
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
