7c83fe8d648ed3311ebd6cb83812943fe7146be3
kontelk <ktelkis@gmail.com>
2023-08-23 10:27:06 +0300

    added specialities, added to vet. closes #27

---
+++ pet-clinic-data/src/main/java/com/kt/sfgpetclinicj8/model/Speciality.java
@@ -0,0 +1,17 @@
+package com.kt.sfgpetclinicj8.model;
+
+/**
+ * Created by kontelk on 8/23/23.
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/kt/sfgpetclinicj8/model/Vet.java
+++ pet-clinic-data/src/main/java/com/kt/sfgpetclinicj8/model/Vet.java
@@ -1,9 +1,19 @@
 package com.kt.sfgpetclinicj8.model;
 
+import java.util.Set;
+
 /**
  * Created by kontelk on 8/15/23.
  */
 public class Vet extends Person {
 
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
